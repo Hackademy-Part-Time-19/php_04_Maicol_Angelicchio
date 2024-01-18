3efb8a97c9799e76103d5f1bdbc7ee43c07a4303
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2024-01-18 19:15:03 +0100

    create classi

---
+++ traccia2.php
@@ -0,0 +1,80 @@
+<?php
+
+class Vertebrates{
+    public function __construct(){
+
+    }
+    protected function print(){
+        echo "Sono un animale vertebrato". PHP_EOL;
+    }
+}
+
+class WarmBlooded extends Vertebrates{
+    public function __contruct(){
+
+    }
+    protected function print(){
+        parent::print();
+        echo "Sono un animale a sangue caldo". PHP_EOL;
+    }
+}
+
+class ColdBlooded extends Vertebrates{
+    public function __contruct(){
+
+    }
+    protected function print(){
+        parent::print();
+        echo "Sono un animale a sangue freddo". PHP_EOL;
+    }
+}
+
+class Mammals extends WarmBlooded{
+    public function __construct(){
+
+    }
+    protected function print(){
+        parent::print();
+        echo "Sono un mammifero".PHP_EOL;
+    }
+}
+
+class Birds extends WarmBlooded{
+    public function __construct(){
+
+    }
+    protected function print(){
+        parent::print();
+        echo "Sono un uccello".PHP_EOL;
+    }
+}
+
+class Fish extends ColdBlooded{
+    public function __construct(){
+
+    }
+    protected function print(){
+        parent::print();
+        echo "Sono un pesce".PHP_EOL;
+    }
+}
+
+class Reptiles extends ColdBlooded{
+    public function __construct(){
+
+    }
+    protected function print(){
+        parent::print();
+        echo "Sono un rettile".PHP_EOL;
+    }
+}
+
+class Amphibians extends ColdBlooded{
+    public function __construct(){
+
+    }
+    protected function print(){
+        parent::print();
+        echo "Sono un anfibio".PHP_EOL;
+    }
+}
